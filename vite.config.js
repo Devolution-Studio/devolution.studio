618972d1399024a2edad9f97fb287ad48631055e
Teo Conan <teo@teoconan.fr>
2023-02-04 00:07:47 +0100

    Configuration dev

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
-import legacy from '@vitejs/plugin-legacy'
+import legacy from '@vitejs/plugin-legacy';
+import { registerSW } from 'virtual:pwa-register';
+
+registerSW({ immediate: true });
 
 export default {
+    server: {
+        open: './src/index.html',
+        port: 8080,
+        strictPort: true,
+        watch: './src/*',
+    },
     plugins: [
         legacy({
             targets: ['defaults', 'not IE 11'],
         }),
+        VitePWA({
+            registerType: 'autoUpdate',
+        }),
     ],
-}
+};
