70e390e7b7ee2427837bd847deffe817137da163
Teo Conan <teo@teoconan.fr>
2023-02-11 12:35:01 +0100

    Sender mail et update env

--- contact.js
+++ contact.js
@@ -1,5 +1,28 @@
-var sendContactMessage = function (name, email, message) {
-    console.log('sendContactMessage', name, email, message);
+const nodemailer = require('nodemailer');
+
+var sendContactMessage = async function (name, email, message) {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASSWORD,
+        },
+    });
+
+    var mailOptions = {
+        from: email,
+        to: 'teo.conan.tirault@gmail.com',
+        subject: name,
+        text: message,
+    };
+
+    try {
+        transporter.sendMail(mailOptions);
+        return true;
+    } catch (e) {
+        console.error(e);
+        return false;
+    }
 };
 
 module.exports = sendContactMessage;
